589c120fd7185a87415b33282f2683d2f6f1cd0b
karinacrmcldev <perfechate@gmail.com>
2022-05-23 22:56:31 +0300

    Creating authentication

---
+++ src/constants/error-messages.ts
@@ -0,0 +1,2 @@
+export const UNAUTHORIZED_ERROR_MESSAGE: string = 'User is not authorized yet';
+// export const FORBIDDEN_ERROR_MESSAGE: string = 'Доступ запрещен';
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,38 +1,52 @@
-import { Entity, Column } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { DataType } from 'sequelize-typescript';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { AppEntity } from './AppEntity';
 
 @Entity()
 export class User extends AppEntity {
-  @Column()
+  @PrimaryGeneratedColumn({ type: 'number' })
+  id: number;
+
+  @Column({ type: 'string', unique: true, nullable: false })
   email: string;
 
-  @Column()
+  @Column({ type: 'string', nullable: false })
   password: string;
 
-  @Column()
+  @Column({ type: 'string', unique: true, nullable: false })
   phone: string;
 
-  @Column()
+  @Column({ type: 'string' })
   avatar: string;
 
-  @Column()
+  @Column({ type: 'string' })
   username: string;
 
-  @Column()
+  @Column({ type: 'string' })
   name: string;
 
-  @Column()
+  @Column({ type: 'string' })
   lastname: string;
 
-  @Column()
+  @Column({ type: 'date' })
   dob: Date;
 
-  @Column()
+  @Column({ type: 'string' })
   nativeLang: string;
 
-  @Column()
+  @Column({ type: 'string' })
   learningLang: string;
 
-  @Column()
+  @Column({ type: 'string' })
   location: string;
+
+  @Column({ type: 'string', default: '', nullable: false })
+  refreshToken: string;
+
+  @Column({ type: 'boolean' })
+  confirmed: boolean;
+
+  @Column({ type: 'string' })
+  confirmed_hash: string;
 }
