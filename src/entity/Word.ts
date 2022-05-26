80a2cd3631669849e492541d319519d90df77775
karinacrmcldev <perfechate@gmail.com>
2022-05-26 22:38:13 +0300

    Added list controller, provider

---
+++ src/entity/Word.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { AppEntity } from './AppEntity';
+import { List } from './List';
+
+@Entity()
+export class Word extends AppEntity {
+  @PrimaryGeneratedColumn({ type: 'number' })
+  id: number;
+
+  @Column({ type: 'string', nullable: false })
+  wordNative: string;
+
+  @Column({ type: 'string', nullable: false })
+  wordLearning: string;
+
+  @Column({ type: 'string' })
+  dateLearned: null | string;
+
+  @ManyToOne(() => List, (list) => list.words)
+  list: List;
+}
--- src/list/list.service.ts
+++ src/list/list.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { List } from 'src/entity/List';
+import { Repository } from 'typeorm';
+import { CreateListDto } from './dto/create-list.dto';
+import { UpdateListDto } from './dto/update-list.dto';
 
 @Injectable()
-export class ListService {}
+export class ListService {
+  constructor(
+    @InjectRepository(List) private listRepository: Repository<List>,
+  ) {}
+
+  async getAllLists(): Promise<List[]> {
+    return await this.listRepository.find();
+  }
+
+  async getListById(id: number): Promise<List> {
+    return await this.listRepository.findOne({ id });
+  }
+
+  async createList(dto: CreateListDto): Promise<List> {
+    return this.listRepository.create(dto);
+  }
+
+  async updateList(id: number, dto: UpdateListDto): Promise<List> {
+    const list = await this.getListById(id);
+
+    if (!list) {
+      throw new HttpException('List not found', 404);
+    }
+
+    return this.listRepository.save({ id, ...dto });
+  }
+
+  async deleteList(id: number): Promise<void> {
+    await this.listRepository.softDelete(id);
+  }
+}
